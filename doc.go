addedef789557ae8277a968a2c0028ff3f3581a8
savaki <matt.ho@gmail.com>
2016-12-05 08:15:55 -0800

    - improving package docs

--- doc.go
+++ doc.go
@@ -1,3 +1,10 @@
 // Package jp offers a highly performant json selector in the style of the jq command line
 //
+// Usage of this package involves the concept of an Op.  An Op is a transformation that converts a []byte into a []byte.
+// To get started, use the Parse function to obtain an Op.
+//
+//     op, err := jq.Parse(".key")
+//
+// This will create an Op that will accept a JSON object in []byte format and return the value associated with "key."
+//
 package jq
